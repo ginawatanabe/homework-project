95923eaf6bc298d713117737664617e63c0d945c
Gina W <watanabegina26@gmail.com>
2016-10-29 14:57:04 -1000

    Changing focus to HTML and CSS first. Making days static in HTML.

--- app/js/calendar.js
+++ app/js/calendar.js
@@ -1,15 +1,66 @@
 window.onload = function() {
-  var day = document.querySelectorAll(".days li");
-  var sidebar = document.getElementById('sidebar');
+  // let calendar = document.querySelectorAll(".days ul");
+  // let sidebar = document.getElementById('sidebar');
+  // let dayArr = [];
+  //
+  // //Month Object Constructor.
+  // function Month(month, numDays, year) {
+  //   this.month = month;
+  //   this.numDays = numDays;
+  //   this.year = year;
+  // }
+  // //Creation of Month objects.
+  // let January = new Month("January",31,2016);
+  // let February = new Month("February",28,2016);
+  // let March = new Month("March",31,2016);
+  // let April = new Month("April",30,2016);
+  // let May = new Month("May",31,2016);
+  // let June = new Month("June",30,2016);
+  // let July = new Month("July",31,2016);
+  // let August = new Month("August",31,2016);
+  // let September = new Month("September",30,2016);
+  // let October = new Month("October",31,2016);
+  // let November = new Month("November",30,2016);
+  // let December = new Month("December",31,2016);
+
+  //Array of Month objects.
+  // let monthArr = [January, February, March, April, May, June, July, August, September, October, November, December];
+
+  // //Input days of the month.
+  // for (i = 0; i < monthArr.length; i++) {
+  //   let month = monthArr[i];
+  //   for (i = 0; i < month.numDays; i++) {
+  //     //Spawn a day for every day of the month and append it to calendar.
+  //     let day = document.createElement('li');
+  //
+  //     //Onclick, add an event div to the sidebar.
+  //     day.addEventListener("click", function() {
+  //       console.log("Hello");
+  //       let thing = document.createElement('div');
+  //       let name = document.createElement('input');
+  //       let start = document.createElement('input');
+  //       let end = document.createElement('input');
+  //       thing.setAttribute('class', 'thing');
+  //       name.setAttribute('class', 'name');
+  //       start.setAttribute('class', 'start');
+  //       end.setAttribute('class', 'end');
+  //       thing.appendChild(name);
+  //       thing.appendChild(start);
+  //       thing.appendChild(end);
+  //       sidebar.appendChild(thing);
+  //       thing.setAttribute("class", "thing");
+  //     })
 
 
-for (var i = 0; i<day.length; i++) {
-  day[i].onclick = function() {
-    console.log("Hello");
-    var thing = document.createElement("div");
-    sidebar.appendChild(thing);
-    thing.setAttribute("class", "thing");
-  }
-}
+  //Conflict Object Constructor
+  // function Event(name, date, month, startTime, endTime, alert, alertTime) {
+  //   this.name = name;
+  //   this.month = month;
+  //   this.date = date;
+  //   this.startTime = startTime;
+  //   this.endTime = endTime;
+  //   this.alert = alert;
+  //   this.alertTime = alertTime;
+  // }
 
 }
